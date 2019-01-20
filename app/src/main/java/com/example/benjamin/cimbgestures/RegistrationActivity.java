89d7d26ad59e4ac9158608cff772abfb7f20e0b3
benzjamin0 <36101433+benjaminlim00@users.noreply.github.com>
2019-01-20 20:08:09 +0800

    commit one and only

--- app/src/main/java/com/example/benjamin/cimbgestures/RegistrationActivity.java
+++ app/src/main/java/com/example/benjamin/cimbgestures/RegistrationActivity.java
@@ -1,57 +1,56 @@
 package com.example.benjamin.cimbgestures;
 
-import android.content.Context;
+
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+//import com.google.firebase.database.DatabaseReference;
+//import com.google.firebase.database.FirebaseDatabase;
 
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 public class RegistrationActivity extends AppCompatActivity {
     private EditText inputUsername, inputPassword;
     private Button exitButton, registerButton;
-    DatabaseReference mRootDatabaseRef = FirebaseDatabase.getInstance().getReference();
-    //StorageReference storageReference = FirebaseStorage.getInstance().getReference();
 
-    DatabaseReference databaseReferencePart1;
+//    DatabaseReference mRootDatabaseRef = FirebaseDatabase.getInstance().getReference();
 
-    String CHILD_NODE_PART1 = "Users";
+//    DatabaseReference databaseReferencePart1;
+
+//    String CHILD_NODE_PART1 = "Users";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registration_activity);
 
+
+
+
         inputUsername = findViewById(R.id.inputUsername);
         inputPassword = findViewById(R.id.inputPassword);
 
-        exitButton = findViewById(R.id.exitButton);
+//        backButton = findViewById(R.id.backButton);
         registerButton = findViewById(R.id.regButton);
 
+        /*
         exitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
-                System.exit(0);
+                Intent i = new Intent(RegistrationActivity.this, RegisterGesture.class);
+                startActivity(i);
             }
-        });
+        });*/
 
-        databaseReferencePart1 = mRootDatabaseRef.child(CHILD_NODE_PART1);
+//        databaseReferencePart1 = mRootDatabaseRef.child(CHILD_NODE_PART1);
+
 
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v) {/*
                 String username = inputUsername.getText().toString().trim();
                 String password = inputPassword.getText().toString().trim();
 
@@ -73,13 +72,19 @@ public class RegistrationActivity extends AppCompatActivity {
                     databaseReferencePart1.child(username).child("password").setValue(String.valueOf(password));
                 } catch (NoSuchAlgorithmException e) {
                     e.printStackTrace();
-                }
+                }*/
 
 
-                startActivity(new Intent(RegistrationActivity.this, RegisterGesture.class));
+                startActivity(new Intent(RegistrationActivity.this, RegistrationDone.class));
             }
         });
     }
+
+
+
+
+
+
 }
     /*
     @Override
--- app/src/main/java/com/example/benjamin/cimbgestures/TransactionSuccess.java
+++ app/src/main/java/com/example/benjamin/cimbgestures/TransactionSuccess.java
@@ -1,19 +1,32 @@
 package com.example.benjamin.cimbgestures;
 
-
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
-public class DonePage extends AppCompatActivity {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.UUID;
+
+public class TransactionSuccess extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.done_page);
+        setContentView(R.layout.success_page);
+
+        Bundle extras = getIntent().getExtras();
+        final String value = extras.getString("valuekey");
+        final String receiverID = extras.getString("receiverkey");
+        final String target = extras.getString("targetkey");
+
+        //we send the string over to firebase
+        String uuid = UUID.randomUUID().toString();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("transactions").child(uuid);
+        myRef.setValue("Benjamin transferred $" + value + " to " + receiverID);
 
 
         //now we countdown 2 sec and app destroy
@@ -30,10 +43,5 @@ public class DonePage extends AppCompatActivity {
                 //System.exit(1);
             }
         }, 2000);
-
-
-
     }
-
-
 }
